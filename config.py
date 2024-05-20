b62de08e44338ca650ecce33b2173b4a3ee12cc0
Pankaj Kumar <138761089+ankaj123r@users.noreply.github.com>
2024-05-20 20:49:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "autorenamethumbnailbot")
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
              # Ask Doubt on telegram @KingVJ01
 
-DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+DB_NAME = os.environ.get("DB_NAME", "renamebot")     
 
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://pankaj1123r:DeFV9aOdch29rLVvPIDD@cluster0.y53pq4k.mongodb.net/?retryWrites=true&w=majority")
  
